3e6a3734c917bac4e1d47e916c8812800179d99c
Robin Singh <44robin.rs@gmail.com>
2023-08-01 18:37:42 +0530

    keyError fixed

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -6,7 +6,6 @@ import { addItem } from "../utils/cartSlice";
 import { useDispatch } from "react-redux";
 import RestaurantProfile from "./RestaurantProfile";
 
-
 const RestaurantMenu = () => {
   const [res, isLoaded] = useRestaurant();
   const restaurantProfileData = res[0]?.card?.card?.info;
@@ -24,19 +23,16 @@ const RestaurantMenu = () => {
     <>
       {/* {console.log(res[2]?.groupedCard?.cardGroupMap?.REGULAR.cards)} */}
       <RestaurantProfile {...restaurantProfileData} />
-      {console.log(menuData)}
+      {/* {console.log(menuData)} */}
       {menuData.map((menu) => {
         const menuCard = menu?.card?.card;
         const title = menu.card?.card?.title;
         const items = menu?.card?.card?.itemCards;
 
         return (
-          <div className="w-3/5 m-auto text-xl">
+          <div className="w-3/5 m-auto text-xl" key={title}>
             {!menuCard?.title ? null : (
-              <div
-                className=" font-bold my-4 px-2 py-4 border-2 border-gray-400"
-                key={title}
-              >
+              <div className=" font-bold my-4 px-2 py-4 border-2 border-gray-400">
                 {title}
               </div>
             )}
@@ -44,19 +40,16 @@ const RestaurantMenu = () => {
             {!items
               ? null
               : items.map((item) => {
-				const {imageId, name, id, price} = item?.card?.info;
-                  return item?.card?.info?.imageId === "" ||
-                  !item?.card?.info?.imageId ? null : (
+                  const { imageId, name, id, price } = item?.card?.info;
+                  return imageId === "" || !imageId ? null : (
                     <div
-                      key={item?.card?.info?.id}
+                      key={id}
                       className="flex gap-5  flex-col-reverse md:flex-row justify-between p-5 mb-5 shadow-md items-center "
                     >
                       <div className="w-3/4">
-                        <p className="text-xl font-bold pb-2">
-                          {item?.card?.info?.name}
-                        </p>
+                        <p className="text-xl font-bold pb-2">{name}</p>
                         <p className="font-bold pb-2">
-                          ₹ {Math.floor(item?.card?.info?.price) / 100}
+                          ₹ {Math.floor(price) / 100}
                         </p>
                         <p className="pb-2">{item?.card?.info?.description}</p>
                         <button
@@ -71,8 +64,8 @@ const RestaurantMenu = () => {
                         src={MENU_IMAGE_ITEM_URL + item?.card.info.imageId}
                       />
                     </div>
-				  )}
-            )}
+                  );
+                })}
           </div>
         );
       })}
