ef932e7a0c771f9953fd22268c6d7437576e9505
Robin Singh <44robin.rs@gmail.com>
2023-08-01 19:37:18 +0530

    device error fixed for restaurant menu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -9,14 +9,16 @@ import RestaurantProfile from "./RestaurantProfile";
 const RestaurantMenu = () => {
   const [res, isLoaded] = useRestaurant();
   const restaurantProfileData = res[0]?.card?.card?.info;
-  const menuData = res[2]?.groupedCard?.cardGroupMap?.REGULAR.cards;
+  const menuData =
+    res[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards ??
+    res[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
   const dispatch = useDispatch();
 
   const handleAdditems = (item) => {
     dispatch(addItem(item));
   };
-  if (!res) return null;
 
+  // console.log(menuData);
   return !isLoaded ? (
     <MenuShimmer />
   ) : (
@@ -24,51 +26,54 @@ const RestaurantMenu = () => {
       {/* {console.log(res[2]?.groupedCard?.cardGroupMap?.REGULAR.cards)} */}
       <RestaurantProfile {...restaurantProfileData} />
       {/* {console.log(menuData)} */}
-      {menuData.map((menu) => {
-        const menuCard = menu?.card?.card;
-        const title = menu.card?.card?.title;
-        const items = menu?.card?.card?.itemCards;
+      {menuData &&
+        menuData.map((menu) => {
+          const menuCard = menu?.card?.card;
+          const title = menu.card?.card?.title;
+          const items = menu?.card?.card?.itemCards;
 
-        return (
-          <div className="w-3/5 m-auto text-xl" key={title}>
-            {!menuCard?.title ? null : (
-              <div className=" font-bold my-4 px-2 py-4 border-2 border-gray-400">
-                {title}
-              </div>
-            )}
+          return (
+            <div className="w-3/5 m-auto text-xl" key={title}>
+              {!menuCard?.title ? null : (
+                <div className=" font-bold my-4 px-2 py-4 border-2 border-gray-400">
+                  {title}
+                </div>
+              )}
 
-            {!items
-              ? null
-              : items.map((item) => {
-                  const { imageId, name, id, price } = item?.card?.info;
-                  return imageId === "" || !imageId ? null : (
-                    <div
-                      key={id}
-                      className="flex gap-5  flex-col-reverse md:flex-row justify-between p-5 mb-5 shadow-md items-center "
-                    >
-                      <div className="w-3/4">
-                        <p className="text-xl font-bold pb-2">{name}</p>
-                        <p className="font-bold pb-2">
-                          ₹ {Math.floor(price) / 100}
-                        </p>
-                        <p className="pb-2">{item?.card?.info?.description}</p>
-                        <button
-                          onClick={() => handleAdditems(item)}
-                          className="border-2 text-white border-white bg-gray-900 px-3 py-2 "
-                        >
-                          + Add to cart
-                        </button>
+              {!items
+                ? null
+                : items.map((item) => {
+                    const { imageId, name, id, price } = item?.card?.info;
+                    return imageId === "" || !imageId ? null : (
+                      <div
+                        key={id}
+                        className="flex gap-5  flex-col-reverse md:flex-row justify-between p-5 mb-5 shadow-md items-center "
+                      >
+                        <div className="w-3/4">
+                          <p className="text-xl font-bold pb-2">{name}</p>
+                          <p className="font-bold pb-2">
+                            ₹ {Math.floor(price) / 100}
+                          </p>
+                          <p className="pb-2">
+                            {item?.card?.info?.description}
+                          </p>
+                          <button
+                            onClick={() => handleAdditems(item)}
+                            className="border-2 text-white border-white bg-gray-900 px-3 py-2 "
+                          >
+                            + Add to cart
+                          </button>
+                        </div>
+                        <img
+                          className="md:w-52 "
+                          src={MENU_IMAGE_ITEM_URL + item?.card.info.imageId}
+                        />
                       </div>
-                      <img
-                        className="md:w-52 "
-                        src={MENU_IMAGE_ITEM_URL + item?.card.info.imageId}
-                      />
-                    </div>
-                  );
-                })}
-          </div>
-        );
-      })}
+                    );
+                  })}
+            </div>
+          );
+        })}
     </>
   );
 };
