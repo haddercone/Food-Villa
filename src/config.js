13b32f221375ea098cb6fe921a399ddf55ba44ac
Robin Singh <44robin.rs@gmail.com>
2023-09-10 10:22:35 +0530

    new URL updated

--- src/config.js
+++ src/config.js
@@ -1,14 +1,13 @@
 export const IMAGE_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
-export const MENU_IMAGE_ITEM_URL ="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/"
+export const MENU_IMAGE_ITEM_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
 export const CROUSEL_IMAGE_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/";
-export const RESTAURANT_MENU_URL = 
-"https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=27.8973944&lng=78.0880129&restaurantId="
-  // "https://food-villa-server.vercel.app/api/restaurant/menu?lat=27.8973944&lng=78.0880129&menuId=";
-export const SWIGGY_RESTAURANT_CDN_URL = 
-"https://food-villa-server.vercel.app/api/restaurants?lat=27.8973944&lng=78.0880129&offset=15&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING"
+export const RESTAURANT_MENU_URL =
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=27.8973944&lng=78.0880129&restaurantId=";
+export const SWIGGY_RESTAURANT_CDN_URL =
+  "https://food-villa-server.vercel.app/api/restaurants?lat=28.4594965&lng=77.0266383&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const SITE_LOGO =
   "https://lh3.googleusercontent.com/Em7AHf7XBH_RtGfCBVXz9RH8SM_pHkj3xPP-yd3cRguY1_Jc8fmqgx6WxnvGVyPV5xs5gL3HCD0FCuv6Xo4CwoY6ak4";
-
